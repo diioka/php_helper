ad8aa6ad93b7afd9494a6e46246c198ac39a5aaf
diioka <idaiki.0202@gmail.com>
2020-09-09 10:33:08 +0900

    fix: rmdirRF

--- helper.php
+++ helper.php
@@ -159,17 +159,11 @@ function echoln($string) {
 function rmdirRF($dir) {
     $files = array_diff(scandir($dir), array('.','..'));
     foreach ($files as $file) {
-        // ファイルかディレクトリによって処理を分ける
         if (is_dir("$dir/$file")) {
-            // ディレクトリなら再度同じ関数を呼び出す
             rmdirRF("$dir/$file");
         } else {
-            // ファイルなら削除
             unlink("$dir/$file");
-            echo "ファイル:" . $dir . "/" . $file . "を削除n";
         }
     }
-    // 指定したディレクトリを削除
-    echo "ディレクトリ:" . $dir . "を削除n";
     return rmdir($dir);
 }
